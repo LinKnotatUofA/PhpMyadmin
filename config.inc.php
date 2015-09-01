cb2d89be69689f716ce1351376a6da1ddab007ad
Sam Bao <sam.b5687@gmail.com>
2015-09-01 01:48:14 -0600

    restoring the config php

--- config.inc.php
+++ config.inc.php
@@ -15,7 +15,7 @@ $cfg['Servers'][$i]['host'] = 'us-cdbr-azure-west-c.cloudapp.net';
 $cfg['Servers'][$i]['port'] = '';
 $cfg['Servers'][$i]['socket'] = '';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
 
 $cfg['Servers'][$i]['pmadb'] = 'bsquared';
 $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
